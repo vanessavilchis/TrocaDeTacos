f8d8b1d0fea45acb4a8f70cc3d640c2ef92c496b
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 13:11:41 -0500

    Commit 26 - Updated using polymorphism, taco drink chips and salsa etc will live under menuitems. added function to add items,  some have no ussages yet.

--- src/main/java/com/pluralsight/trocadetacos/models/Order.java
+++ src/main/java/com/pluralsight/trocadetacos/models/Order.java
@@ -1,24 +1,51 @@
 package com.pluralsight.trocadetacos.models;
 import java.util.ArrayList;
+
 public class Order {
-    private ArrayList<Taco> tacos;
-    private ArrayList<Drink> drinks;
-    private ArrayList<ChipsAndSalsa> chips;
-
+    private ArrayList<MenuItem> items;
+    //technically my constructor here:
     public Order() {
-        this.tacos = new ArrayList<>();
-        this.drinks = new ArrayList<>();
-        this.chips = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
-
+    public void addTaco(Taco taco) {
+        items.add(taco);
+    }
+    public void addDrink(Drink drink) {
+        items.add(drink);
+    }
+    public void addChips(ChipsAndSalsa chipsAndSalsa) {
+        items.add(chipsAndSalsa);
+    }
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
     public ArrayList<Taco> getTacos() {
+        ArrayList<Taco> tacos = new ArrayList<>();
+        for (MenuItem item : items) {
+            if (item instanceof Taco) {
+                tacos.add((Taco) item);
+            }
+        }
         return tacos;
     }
     public ArrayList<Drink> getDrinks() {
+        ArrayList<Drink> drinks = new ArrayList<>();
+        for (MenuItem item : items) {
+            if (item instanceof Drink) {
+                drinks.add((Drink) item);
+            }
+        }
         return drinks;
     }
-
     public ArrayList<ChipsAndSalsa> getChips() {
+        ArrayList<ChipsAndSalsa> chips = new ArrayList<>();
+        for (MenuItem item : items) {
+            if (item instanceof ChipsAndSalsa) {
+                chips.add((ChipsAndSalsa) item);
+            }
+        }
         return chips;
     }
-}
