bad0c1c961d91b364a5980f6e937ba4d0512808f
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 10:27:48 -0500

    Commit 6 - Topping constructor + getters and setters

--- src/main/java/com/pluralsight/trocadetacos/models/Topping.java
+++ src/main/java/com/pluralsight/trocadetacos/models/Topping.java
@@ -1,4 +1,36 @@
 package com.pluralsight.trocadetacos.models;
 
 public class Topping {
+    private String name;
+    private String category;
+    private boolean isExtra;
+
+    public Topping(String name, String category) {
+        this.name = name;
+        this.category = category;
+        this.isExtra = false;
+    }
+    public Topping(String name, String category, boolean isExtra) {
+        this.name = name;
+        this.category = category;
+        this.isExtra = isExtra;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public boolean isExtra() {
+        return isExtra;
+    }
+    public void setExtra(boolean extra) {
+        isExtra = extra;
+    }
 }
