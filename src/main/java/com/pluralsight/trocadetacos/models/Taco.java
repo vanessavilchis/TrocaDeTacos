a1e14479e6e5d7226a10ff754403ac4a64d9c617
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 11:40:54 -0500

    Commit 22 - Taco Stringbuilder customization, implemented customize() from customizable interface to return info about taco customizations like it's options of tortilla type, deep fried, the size, toppings. added toString for formatted display.

--- src/main/java/com/pluralsight/trocadetacos/models/Taco.java
+++ src/main/java/com/pluralsight/trocadetacos/models/Taco.java
@@ -85,20 +85,63 @@ public class Taco extends MenuItem implements Customizable {
 
         return sb.toString();
     }
+    @Override
+    public String customize() {
+        StringBuilder customization = new StringBuilder();
+        customization.append("Taco Customization:\n");
+        customization.append("  Size: ").append(size).append("\n");
+        customization.append("  Tortilla: ").append(tortillaType).append("\n");
+        customization.append("  Deep Fried: ").append(isDeepFried ? "Yes" : "No").append("\n");
 
+        if (!toppings.isEmpty()) {
+            customization.append("  Toppings:\n");
+            for (Topping topping : toppings) {
+                customization.append("    - ").append(topping.toString()).append("\n");
+            }
+        }
+
+        return customization.toString();
+    }
+    @Override
     public double getPrice() {
-
         double basePrice = 0.0;
         switch (size) {
             case "Single":
                 basePrice = 3.50;
                 break;
-            case "3 Taco":
+            case "3-Taco":
                 basePrice = 9.00;
                 break;
             case "Burrito":
                 basePrice = 8.50;
                 break;
         }
+
+        double toppingTotal = 0.0;
+        for (Topping topping : toppings) {
+            toppingTotal += topping.getPrice(size);
+        }
+
+        return basePrice + toppingTotal;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(size).append(" - ").append(tortillaType);
+
+        if (isDeepFried) {
+            sb.append(" (DEEP FRIED)");
+        }
+
+        if (!toppings.isEmpty()) {
+            sb.append("\n  Toppings:");
+            for (Topping topping : toppings) {
+                sb.append("\n    - ").append(topping.toString());
+            }
+        }
+
+        sb.append("\n  Price: $").append(String.format("%.2f", getPrice()));
+
+        return sb.toString();
     }
 }
