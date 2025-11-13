89bc594365c9c08b9c58ec0f7e516744b5151c82
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 12:04:53 -0500

    Commit 25 - over ride toString for displaying chips and salsa formatted. inheritance added, extended menuitem and implement customizable interface. provides getdescription, customize and getPrice abstract and interfaces.

--- src/main/java/com/pluralsight/trocadetacos/models/ChipsAndSalsa.java
+++ src/main/java/com/pluralsight/trocadetacos/models/ChipsAndSalsa.java
@@ -1,24 +1,35 @@
 package com.pluralsight.trocadetacos.models;
 
-public class ChipsAndSalsa {
+public class ChipsAndSalsa extends MenuItem implements Customizable {
+
     private String salsaType;
     private static final double price = 1.50;
 
     public ChipsAndSalsa(String salsaType) {
+        super("Chips & " + salsaType + " Salsa");
         this.salsaType = salsaType;
     }
-
     public String getSalsaType() {
         return salsaType;
     }
-
     public void setSalsaType(String salsaType) {
         this.salsaType = salsaType;
     }
     public double getPrice() {
         return price;
     }
-
+    @Override
+    public String getDescription() {
+        return "Chips & " + salsaType + " Salsa";
+    }
+    @Override
+    public String customize() {
+        StringBuilder customization = new StringBuilder();
+        customization.append("Chips & Salsa Customization:\n");
+        customization.append("  Salsa Type: ").append(salsaType).append("\n");
+        customization.append("  Price: $").append(String.format("%.2f",price)).append("\n");
+        return customization.toString();
+    }
     @Override
     public String toString() {
         return "Chips & " + salsaType + " Salsa - $" + String.format("%.2f", getPrice());
