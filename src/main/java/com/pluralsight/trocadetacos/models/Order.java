69da1082c5c7c5971203d41101316048445ec1ed
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 10:31:35 -0500

    Commit 7 - Order constructor ArrayList + getters and setters

--- src/main/java/com/pluralsight/trocadetacos/models/Order.java
+++ src/main/java/com/pluralsight/trocadetacos/models/Order.java
@@ -1,4 +1,24 @@
 package com.pluralsight.trocadetacos.models;
-
+import java.util.ArrayList;
 public class Order {
+    private ArrayList<Taco> tacos;
+    private ArrayList<Drink> drinks;
+    private ArrayList<ChipsAndSalsa> chips;
+
+    public Order() {
+        this.tacos = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
+    }
+
+    public ArrayList<Taco> getTacos() {
+        return tacos;
+    }
+    public ArrayList<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public ArrayList<ChipsAndSalsa> getChips() {
+        return chips;
+    }
 }
