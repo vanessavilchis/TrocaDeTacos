ba1eb26d04d5c0711d13bdbd563f1a4eac683837
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 10:21:07 -0500

    Commit 4 - Chips and Salsa Constructor and getters and setters

--- src/main/java/com/pluralsight/trocadetacos/models/ChipsAndSalsa.java
+++ src/main/java/com/pluralsight/trocadetacos/models/ChipsAndSalsa.java
@@ -1,4 +1,18 @@
 package com.pluralsight.trocadetacos.models;
 
 public class ChipsAndSalsa {
+    private String salsaType;
+    private static final double price = 1.50;
+
+    public ChipsAndSalsa(String salsaType) {
+        this.salsaType = salsaType;
+    }
+
+    public String getSalsaType() {
+        return salsaType;
+    }
+
+    public void setSalsaType(String salsaType) {
+        this.salsaType = salsaType;
+    }
 }
