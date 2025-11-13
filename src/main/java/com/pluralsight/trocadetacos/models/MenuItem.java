97c35cb67d0ceb5749911ed7aa6c8b916fba033e
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-13 11:29:00 -0500

    Commit 20 - Abstract menuitem class

--- src/main/java/com/pluralsight/trocadetacos/models/MenuItem.java
+++ src/main/java/com/pluralsight/trocadetacos/models/MenuItem.java
@@ -1,4 +1,18 @@
 package com.pluralsight.trocadetacos.models;
 
-public class MenuItem {
-}
+public abstract class MenuItem implements Priceable {
+    protected String name;
+    public MenuItem(String name) {
+        this.name = name;
+    }
+    public abstract String getDescription();
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void displayInfo() {
+        System.out.println(getDescription());
+        System.out.printf("Price: $%.2f%n", getPrice());
+    }
